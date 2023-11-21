6cc4c432d2957a00602de82bfb86f5509a51edea
filliperomero <fillipe.romero@gmail.com>
2023-11-21 00:26:18 -0700

    Add FunctionLibrary

---
+++ Source/Platformer/Private/PFFunctionLibrary.cpp
@@ -0,0 +1,12 @@
+﻿// Copyright Fillipe Romero
+
+#include "PFFunctionLibrary.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+void UPFFunctionLibrary::PlayBlockInteractFX(const UObject* WorldContextObject, USoundBase* SoundEffect, UNiagaraSystem* BlockEffect, UParticleSystem* ParticleEffect, FVector EffectLocation)
+{
+	UGameplayStatics::PlaySound2D(WorldContextObject, SoundEffect);
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(WorldContextObject, BlockEffect, EffectLocation);
+	UGameplayStatics::SpawnEmitterAtLocation(WorldContextObject, ParticleEffect, EffectLocation);
+}
---
+++ Source/Platformer/Public/PFFunctionLibrary.h
@@ -0,0 +1,17 @@
+﻿// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PFFunctionLibrary.generated.h"
+
+UCLASS()
+class PLATFORMER_API UPFFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "PFFunctionLibrary|Block")
+	static void PlayBlockInteractFX(const UObject* WorldContextObject, USoundBase* SoundEffect, UNiagaraSystem* BlockEffect, UParticleSystem* ParticleEffect, FVector EffectLocation);
+};

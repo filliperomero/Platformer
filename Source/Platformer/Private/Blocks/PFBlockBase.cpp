a5576a2a88fca41213eb61a2c52f2b5293eefcd2
filliperomero <fillipe.romero@gmail.com>
2023-11-21 23:20:30 -0700

    Add Base Block

---
+++ Source/Platformer/Private/Blocks/PFBlockBase.cpp
@@ -0,0 +1,33 @@
+﻿// Copyright Fillipe Romero
+
+#include "Blocks/PFBlockBase.h"
+
+#include "Interface/PlayerInterface.h"
+
+APFBlockBase::APFBlockBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	
+	BlockMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BlockMesh"));
+	BlockMesh->SetupAttachment(RootComponent);
+}
+
+void APFBlockBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BlockMesh->OnComponentHit.AddDynamic(this, &ThisClass::OnMeshHit);
+}
+
+void APFBlockBase::OnMeshHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (!OtherActor->Implements<UPlayerInterface>() || Hit.ImpactNormal.Z != 1) return;
+
+	HitBlock();
+}
+
+void APFBlockBase::HitBlock()
+{
+}
---
+++ Source/Platformer/Public/Blocks/PFBlockBase.h
@@ -0,0 +1,48 @@
+﻿// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PFBlockBase.generated.h"
+
+class UNiagaraSystem;
+
+UCLASS()
+class PLATFORMER_API APFBlockBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APFBlockBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnMeshHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	virtual void HitBlock();
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> BlockMesh;
+
+	bool bIsFinishedInteracting = false;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	int32 AmountOfCoins = 1;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	int32 AmountOfPoints = 10;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	TObjectPtr<UNiagaraSystem> InteractEffect;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	TObjectPtr<UParticleSystem> InteractParticleEffect;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	TObjectPtr<USoundBase> SoundEffect;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	TObjectPtr<UMaterial> InteractMaterial;
+};

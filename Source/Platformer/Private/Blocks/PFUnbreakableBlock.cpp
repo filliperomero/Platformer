90458a9d9ad3a468528e0330ba12fa6529f9c147
filliperomero <fillipe.romero@gmail.com>
2023-11-26 00:46:08 -0700

    Rename file from FakeBreakableBlock to UnbreakableBlock and add ability to start as invisible

---
+++ Source/Platformer/Private/Blocks/PFUnbreakableBlock.cpp
@@ -0,0 +1,88 @@
+﻿// Copyright Fillipe Romero
+
+#include "Blocks/PFUnbreakableBlock.h"
+#include "PFFunctionLibrary.h"
+#include "Components/BoxComponent.h"
+#include "Interface/PlayerInterface.h"
+#include "Kismet/GameplayStatics.h"
+
+APFUnbreakableBlock::APFUnbreakableBlock()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetupAttachment(BlockMesh);
+	BoxCollision->SetBoxExtent(FVector(100.f, 50.f, 100.f));
+	BoxCollision->SetRelativeLocation(FVector(0.f, 0.f, 51.f));
+}
+
+void APFUnbreakableBlock::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsInvisible)
+	{
+		BlockMesh->SetVisibility(false);
+		BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnBoxBeginOverlap);
+		BoxCollision->OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnBoxEndOverlap);
+	}
+	else
+	{
+		BoxCollision->Deactivate();
+	}
+}
+
+void APFUnbreakableBlock::HitBlock(AActor* TargetActor)
+{
+	Super::HitBlock(TargetActor);
+	
+	if (AmountOfInteract >= MaxAmountOfInteract)
+	{
+		if (NotAbleToInteractSound) UGameplayStatics::PlaySound2D(this, NotAbleToInteractSound);
+
+		return;
+	}
+
+	if (IsInvisible && !BlockMesh->IsVisible())
+	{
+		BlockMesh->SetVisibility(true);
+		if (BoxCollision) BoxCollision->DestroyComponent();
+	}
+
+	const bool IsLastInteract = AmountOfInteract + 1 >= MaxAmountOfInteract;
+	
+	FVector Location = GetActorLocation();
+	Location.Z += 50;
+	
+	UPFFunctionLibrary::PlayBlockInteractFX(this, SoundEffect, nullptr, InteractParticleEffect, Location);
+	
+	if (InteractMaterial && IsLastInteract)
+		BlockMesh->SetMaterial(0, InteractMaterial);
+	
+	ShowFloatingPoints(TargetActor);
+
+	IPlayerInterface::Execute_AddToPoints(TargetActor, AmountOfPoints);
+	IPlayerInterface::Execute_AddToCoins(TargetActor, AmountOfCoins);
+
+	// For Future: Change this for an Niagara System
+	if (CoinToSpawn)
+		GetWorld()->SpawnActor<AActor>(CoinToSpawn, GetActorTransform());
+
+	AnimateBlock();
+	
+	AmountOfInteract++;
+}
+
+void APFUnbreakableBlock::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!OtherActor->Implements<UPlayerInterface>()) return;
+
+	BlockMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void APFUnbreakableBlock::OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (!OtherActor->Implements<UPlayerInterface>()) return;
+
+	BlockMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
---
+++ Source/Platformer/Public/Blocks/PFUnbreakableBlock.h
@@ -0,0 +1,50 @@
+﻿// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PFBlockBase.h"
+#include "PFUnbreakableBlock.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class PLATFORMER_API APFUnbreakableBlock : public APFBlockBase
+{
+	GENERATED_BODY()
+
+public:
+	APFUnbreakableBlock();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void HitBlock(AActor* TargetActor) override;
+
+	UFUNCTION()
+	virtual void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void AnimateBlock();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UBoxComponent> BoxCollision;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	TSubclassOf<AActor> CoinToSpawn;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	TObjectPtr<USoundBase> NotAbleToInteractSound;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	int32 MaxAmountOfInteract = 1;
+
+	UPROPERTY(EditAnywhere, Category = "Block Properties")
+	bool IsInvisible = false;
+
+private:
+	int32 AmountOfInteract = 0;
+};

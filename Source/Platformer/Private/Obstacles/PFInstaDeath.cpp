d18ce03fe1212942b74fed4efbf3f56294c0c978
filliperomero <fillipe.romero@gmail.com>
2023-12-09 23:01:08 -0700

    Add InstaDeath obstacle

---
+++ Source/Platformer/Private/Obstacles/PFInstaDeath.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright Fillipe Romero
+
+#include "Obstacles/PFInstaDeath.h"
+#include "Components/BoxComponent.h"
+#include "Interface/PlayerInterface.h"
+
+APFInstaDeath::APFInstaDeath()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	SetRootComponent(BoxCollision);
+	BoxCollision->SetBoxExtent(FVector(400.f, 400.f, 50.f));
+	BoxCollision->SetLineThickness(3.f);
+}
+
+void APFInstaDeath::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnBoxCollisionBeginOverlap);
+}
+
+void APFInstaDeath::OnBoxCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!OtherActor->Implements<UPlayerInterface>()) return;
+
+	IPlayerInterface::Execute_AddHitPoints(OtherActor, -99999);
+}
---
+++ Source/Platformer/Public/Obstacles/PFInstaDeath.h
@@ -0,0 +1,28 @@
+﻿// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PFInstaDeath.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class PLATFORMER_API APFInstaDeath : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APFInstaDeath();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	virtual void OnBoxCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UBoxComponent> BoxCollision;
+};

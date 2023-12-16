301720524aa413550301525c0e5c2871a021e2a2
filliperomero <fillipe.romero@gmail.com>
2023-12-15 22:43:35 -0700

    Add Clouds

---
+++ Source/Platformer/Private/Actors/PFClouds.cpp
@@ -0,0 +1,39 @@
+﻿// Copyright Fillipe Romero
+
+#include "Actors/PFClouds.h"
+
+#include "Components/BoxComponent.h"
+#include "Components/InstancedStaticMeshComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+APFClouds::APFClouds()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+
+	Clouds = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Clouds"));
+	Clouds->SetupAttachment(RootComponent);
+	Clouds->SetRelativeRotation(FRotator(0.f, 0.f, 90.f));
+	Clouds->SetCastShadow(false);
+	
+	SpawnArea = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnArea"));
+	SpawnArea->SetupAttachment(RootComponent);
+	SpawnArea->SetBoxExtent(FVector(5000.f, 1000.f, 2000.f));
+}
+
+void APFClouds::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	
+	Clouds->ClearInstances();
+
+	for (int32 Index = 0; Index < AmountOfClouds; Index++)
+	{
+		FVector InstanceLocation = UKismetMathLibrary::RandomPointInBoundingBox(SpawnArea->GetRelativeLocation(), SpawnArea->GetUnscaledBoxExtent());
+		const float CloudScale = FMath::RandRange(CloudMinScale, CloudMaxScale);
+		const FTransform& InstanceTransform = FTransform(FRotator(), InstanceLocation, FVector(CloudScale));
+		
+		Clouds->AddInstance(InstanceTransform);
+	}
+}
---
+++ Source/Platformer/Public/Actors/PFClouds.h
@@ -0,0 +1,35 @@
+﻿// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PFClouds.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class PLATFORMER_API APFClouds : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APFClouds();
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UBoxComponent> SpawnArea;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInstancedStaticMeshComponent> Clouds;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Clouds Properties")
+	int32 AmountOfClouds = 25;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Clouds Properties")
+	float CloudMinScale = 3;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Clouds Properties")
+	float CloudMaxScale = 10;
+};
